639180687601c7355ec440e90f69bd05d768d34f
Jay Freeman (saurik) <saurik@saurik.com>
2009-09-16 04:39:34 +0000

    Some minor details.

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@
 flags=(-O2 -g0 -fno-exceptions -fvisibility=hidden)
 cycc -i2.0 -oMobileSafety.dylib -- "${flags[@]}" -dynamiclib MobileSafety.mm \
     -framework CoreFoundation -framework Foundation -framework UIKit \
-    -L. -lsubstrate -lobjc -init _MSInitialize
+    -L. -lsubstrate -lobjc
