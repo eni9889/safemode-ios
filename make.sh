9065bb4466b38159a8fdfd57ce047ed27ab647f2
Jay Freeman (saurik) <saurik@saurik.com>
2011-10-13 13:06:42 -0700

    Refactor SafeMode to a separate package.

--- make.sh
+++ make.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-flags=(-O2 -g0 -fno-exceptions -fvisibility=hidden)
-cycc -i2.0 -oMobileSafety.dylib -- "${flags[@]}" -dynamiclib MobileSafety.mm \
-    -framework CoreFoundation -framework Foundation -framework UIKit \
-    -L. -lsubstrate -lobjc
+CYCC_APT_VERSION=$(./version.sh) cycc -i2.0 -s -p MobileSafety.mm
